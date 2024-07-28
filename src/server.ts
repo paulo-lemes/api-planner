675db784279f65e1e54f3851dd707dddb3811bf7
Paulo Lemes <paulo-lemes@live.com>
2024-07-27 21:04:56 -0300

    Added initial configs

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import cors from "@fastify/cors";
+import fastify from "fastify";
+import {
+  serializerCompiler,
+  validatorCompiler,
+} from "fastify-type-provider-zod";
+import { env } from "./env";
+
+const app = fastify();
+
+app.register(cors, {
+  origin: "*",
+});
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.listen({ port: env.PORT }).then(() => {
+  console.log("Server running!");
+});

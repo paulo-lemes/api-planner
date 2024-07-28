191ea78809709a2bdecd2048a4b0504ad4690b0d
Paulo Lemes <paulo-lemes@live.com>
2024-07-27 21:12:26 -0300

    Created create-trip route

---
+++ src/routes/create-trip.ts
@@ -0,0 +1,44 @@
+import type { FastifyInstance } from "fastify";
+import type { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+import { prisma } from "../lib/prisma";
+
+export async function createTrip(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    "/trips",
+    {
+      schema: {
+        body: z.object({
+          destination: z.string().min(4),
+          starts_at: z.coerce.date(),
+          ends_at: z.coerce.date(),
+          owner_name: z.string(),
+          owner_email: z.string().email(),
+          emails_to_invite: z.array(z.string().email()),
+        }),
+      },
+    },
+    async (request) => {
+      const {
+        destination,
+        starts_at,
+        ends_at,
+        owner_name,
+        owner_email,
+        emails_to_invite,
+      } = request.body;
+
+      const trip = await prisma.trip.create({
+        data: {
+          destination,
+          starts_at,
+          ends_at,
+        },
+      });
+
+      return {
+        tripId: trip.id,
+      };
+    }
+  );
+}

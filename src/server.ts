f81fe244d9d9dcd263bc65cb6b04952cbb023110
Paulo Lemes <paulo-lemes@live.com>
2024-07-30 12:29:06 -0300

    Registered confirm participant route to server

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import {
   validatorCompiler,
 } from "fastify-type-provider-zod";
 import { env } from "./env";
+import { confirmParticipant } from "./routes/confirm-participant";
 import { confirmTrip } from "./routes/confirm-trip";
 import { createTrip } from "./routes/create-trip";
 
@@ -19,6 +20,7 @@ app.setSerializerCompiler(serializerCompiler);
 
 app.register(createTrip);
 app.register(confirmTrip);
+app.register(confirmParticipant);
 
 app.listen({ port: env.PORT }).then(() => {
   console.log("Server running!");
